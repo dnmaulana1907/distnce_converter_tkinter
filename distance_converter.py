ec12e3701ddd9ec3c8edf40247c63f7ea008044f
Dani Maulana <112448031+dnmaulana1907@users.noreply.github.com>
2025-04-04 22:45:46 +0700

    implement class inherit

--- distance_converter.py
+++ distance_converter.py
@@ -2,25 +2,54 @@ import tkinter as tk
 from tkinter import ttk
 from termcolor import colored
 
-root = tk.Tk()
 
-root.title("Distance Converter")
+class distance_converter(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Distance Converter")
+        frame = meters_to_feet(self, padding=(60, 30))
+        frame.grid()
+
+        self.bind("<Return>", frame.calculate_feet)
+class meters_to_feet(ttk.Frame):
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
+        self.feet_value = tk.StringVar()
+        self.meters_value = tk.StringVar()
+
+
+        meters_label = ttk.Label(self, text="Meters:")
+        meters_input = ttk.Entry(self, width=10, textvariable=self.meters_value, font=("Segoe UI", 10))
+        feet_label = ttk.Label(self, text="Feet:")
+        feet_display = ttk.Label(self, textvariable= self.feet_value)
+        calc_button = ttk.Button(self, text="Calculate", command= self.calculate_feet)
+
+        meters_label.grid(row=0, column=0, sticky="W")
+        meters_input.grid(row=0, column=1, sticky="EW")
+        meters_input.focus()
+
+        feet_label.grid(row=1, column=0, sticky="W")
+        feet_display.grid(row=1, column=1, sticky="EW")
+        calc_button.grid(row=2, column=0, columnspan=2, sticky="EW")
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=10, pady=10)
+
+    def calculate_feet(self, *args):
+        try:
+            meters = float(self.meters_value.get())
+            feet = meters * 3.28084
+            self.feet_value.set(f"value is {feet:.2f}")
+            print(colored(f"{meters} meters is equal to {feet:.2f} feet.", 'white', 'on_yellow'))
+        except ValueError:
+            self.feet_value.set("Invalid input")
+            print(colored("Empty Value.... Please enter the value", 'white', 'on_red'))
+
+            pass
+
+
+root = distance_converter()
 root.resizable(False, False)
-
-meters_value = tk.StringVar()
-feet_value = tk.StringVar(value="Feet shown here")
-
-def calculate_feet(*args):
-    try:
-        meters = float(meters_value.get())
-        feet = meters * 3.28084
-        feet_value.set(f"value is {feet:.2f}")
-        print(colored(f"{meters} meters is equal to {feet:.2f} feet.", 'white', 'on_yellow'))
-    except ValueError:
-        feet_value.set("Invalid input")
-        print(colored("Empty Value.... Please enter the value", 'white', 'on_red'))
-
-        pass
 
 def on_closing(*args):
     print(colored("Closing Window", 'white', 'on_red'))
@@ -30,30 +59,4 @@ root.protocol("WM_DELETE_WINDOW", on_closing)
 # stay in middle of screen
 root.columnconfigure(0, weight=1)
 
-
-main = ttk.Frame(root, padding=(30, 15))
-main.grid()
-
-# widgets
-meters_label = ttk.Label(main, text="Meters:")
-meters_input = ttk.Entry(main, width=10, textvariable=meters_value, font=("Segoe UI", 10))
-
-feet_label = ttk.Label(main, text="Feet:")
-feet_display = ttk.Label(main, textvariable= feet_value)
-
-calc_button = ttk.Button(main, text="Calculate", command=calculate_feet)
-
-# grid layout
-meters_label.grid(row=0, column=0, sticky="W")
-meters_input.grid(row=0, column=1, sticky="EW")
-meters_input.focus()
-
-feet_label.grid(row=1, column=0, sticky="W")
-feet_display.grid(row=1, column=1, sticky="EW")
-calc_button.grid(row=2, column=0, columnspan=2, sticky="EW")
-
-for child in main.winfo_children():
-    child.grid_configure(padx=10, pady=10)
-
-root.bind("<Return>", calculate_feet)
 root.mainloop()
